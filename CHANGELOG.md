ca94109ca490d09d0bc2e94f92f232047b4165e5
Louis <spalax@gresille.org>
2020-06-11 20:56:41 +0200

    Drop python3.5 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 * stuffcounter 0.0.0 (unreleased)
 
     * Python support
-      * Drop python3.6 support.
+      * Drop python3.5 and python3.6 support.
       * Add python3.8 support.
 
     -- Louis Paternault <spalax+python@gresille.org>
--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Documentation :: Sphinx",
